3d9b811ea50e0f431f8ef6d255f10422048ff03d
Le Duy Lin <lin.le.set18@eiu.edu.vn>
2023-06-02 13:55:27 +0700

    initialize greatest common devisor

---
+++ Leetcode/LeetCode 75 - Trending Probs/Greatest Common Divisor of Strings/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Greatest_Common_Divisor_of_Strings
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
