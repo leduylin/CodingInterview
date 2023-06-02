3fcf51661c62cd66e7d7315d0c1b48b8d80ce581
Le Duy Lin <lin.le.set18@eiu.edu.vn>
2023-06-02 10:12:59 +0700

    AC remove duplicates from sorted array easy

---
+++ Leetcode/Top Interview 150/Remove Duplicates from Sorted Array/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Remove_Duplicates_from_Sorted_Array
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+
+        public int RemoveDuplicates(int[] nums)
+        {
+            var res = (from num in nums
+                       select num).Distinct().ToList();
+            for (int i = 0; i < res.Count; i++)
+            {
+                nums[i] = res[i];
+            }
+            return res.Count;
+
+        }
+    }
+}
