6ce490102aaf1ab5d4d269a4bd8f0a79a085abb3
Le Duy Lin <lin.le.set18@eiu.edu.vn>
2023-06-02 10:50:19 +0700

    AC - Majority_Element

---
+++ Leetcode/Top Interview 150/Majority Element/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Majority_Element
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+
+        public int MajorityElement(int[] nums)
+        {
+            int standard = (int)Math.Floor((double)nums.Length / 2);
+            var groupNum = nums.GroupBy(i => i);
+            int res = 0;
+            foreach (var keyVal in groupNum)
+            {
+                if (keyVal.Count() > standard)
+                {
+                    res = keyVal.Key;
+                    break;
+                }
+            }
+            return res;
+        }
+    }
+}
