a9cdf38380d364dace49f1a8a04c25de3fb72d5e
Le Duy Lin <lin.le.set18@eiu.edu.vn>
2023-06-04 17:34:56 +0700

    AC - Intersection of two arrays

---
+++ Leetcode/Other/Intersection of Two Arrays II/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace Intersection_of_Two_Arrays_II
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+
+        public int[] Intersect(int[] nums1, int[] nums2)
+        {
+            var num1Dictionary = new Dictionary<int, int>();
+            foreach (int i in nums1)
+            {
+                if (num1Dictionary.ContainsKey(i))
+                {
+                    num1Dictionary[i]++;
+                }
+                else
+                {
+                    num1Dictionary[i] = 1;
+                }
+            }
+
+            var listIntersect = new List<int>();
+            foreach (int i in nums2)
+            {
+                if (num1Dictionary.ContainsKey(i))
+                {
+                    listIntersect.Add(i);
+                    if (num1Dictionary[i] == 1)
+                    {
+                        num1Dictionary.Remove(i);
+                    }
+                    else
+                    {
+                        num1Dictionary[i]--;
+                    }
+                }
+            }
+            return listIntersect.ToArray();
+
+        }
+    }
+}
