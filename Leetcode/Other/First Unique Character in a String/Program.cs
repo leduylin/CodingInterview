7f41c89d98f77465de8fa3ed579b1b073d2341ee
Le Duy Lin <lin.le.set18@eiu.edu.vn>
2023-06-04 17:20:30 +0700

    AC - First unique character

---
+++ Leetcode/Other/First Unique Character in a String/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace First_Unique_Character_in_a_String
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+
+        public int FirstUniqChar(string s)
+        {
+            int result = -1;
+            Dictionary<char, int> charExist = new Dictionary<char, int>();
+            foreach (char c in s)
+            {
+                if (charExist.ContainsKey(c))
+                {
+                    charExist[c]++;
+                }
+                else
+                {
+                    charExist.Add(c, 1);
+                }
+            }
+            int count = 0;
+            for (int i = 0; i < s.Length; i++)
+            {
+                count = charExist[s[i]];
+                if (count == 1)
+                {
+                    result = i;
+                    break;
+                }
+            }
+            return result;
+        }
+    }
+}
