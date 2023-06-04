881bb8a8289aab92018e86a7dad0530bde3d5679
Le Duy Lin <lin.le.set18@eiu.edu.vn>
2023-06-04 17:46:39 +0700

    AC - Contains Duplicate

---
+++ Leetcode/Other/Contains Duplicate/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Contains_Duplicate
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+
+        public bool ContainsDuplicate(int[] nums)
+        {
+            var distinct = nums.Distinct();
+            if (distinct.Count() == nums.Length)
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
